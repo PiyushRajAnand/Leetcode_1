8b34bd47e2ba159a1e6c17e2cb6325c277ab0293
Piyush Raj Anand <piyushrajanand03@gmail.com>
2026-07-13 12:56:22 +0530

    Time: 3 ms (98.42%), Space: 46.3 MB (36.5%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,21 +14,26 @@
  * }
  */
 public class Solution {
+    int ans=0;
+    HashMap<Long,Integer> map=new HashMap<>();
     public int pathSum(TreeNode root, int sum) {
-        if (root == null) return 0;
-        return pathSumFrom(root,(long) sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
-    }
-    
-    private int pathSumFrom(TreeNode node, long sum) {
-        if(node==null){
+        if(root==null){
             return 0;
         }
-        int ans=0;
-        if(node.val==sum){
-            ans++;
-        }
-        ans+=pathSumFrom(node.left,sum-node.val);
-        ans+=pathSumFrom(node.right,sum-node.val);
+        map.put(0L,1);
+        pathSumFrom(root,0,map,sum);
         return ans;
+    }
+    
+    private void pathSumFrom(TreeNode node, long sum,HashMap<Long,Integer> map,int target) {
+        if(node==null){
+            return;
+        }
+        sum+=node.val;
+        ans+=map.getOrDefault(sum-target,0);
+        map.put(sum,map.getOrDefault(sum,0)+1);
+        pathSumFrom(node.left,sum,map,target);
+        pathSumFrom(node.right,sum,map,target);
+        map.put(sum,map.get(sum)-1);
     }
 }
