5d2d3352e82f00d618015b516c13ae0fb72a87fc
Piyush Raj Anand <piyushrajanand03@gmail.com>
2026-07-24 01:21:48 +0530

    Time: 0 ms (100%), Space: 46.6 MB (17.53%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -24,23 +24,37 @@ class Node {
 class Solution {
     
     public Node connect(Node root) {
+        // if(root==null){
+        //     return root;
+        // }
+        // Queue<Node> queue=new LinkedList<>();
+        // queue.offer(root);
+        // while(!queue.isEmpty()){
+        //     Node right=null;
+        //     for(int i=queue.size();i>0;i--){
+        //         Node curr=queue.poll();
+        //         curr.next=right;
+        //         right=curr; //Give me set the current pointer
+        //         if(curr.right!=null){
+        //             queue.offer(curr.right);
+        //             queue.offer(curr.left);
+        //         }
+        //     }
+        // }
+        // return root;
+
         if(root==null){
-            return root;
+            return null;
         }
-        Queue<Node> queue=new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            Node right=null;
-            for(int i=queue.size();i>0;i--){
-                Node curr=queue.poll();
-                curr.next=right;
-                right=curr;
-                if(curr.right!=null){
-                    queue.offer(curr.right);
-                    queue.offer(curr.left);
-                }
+        if(root.left!=null){
+            root.left.next=root.right;
+            if(root.next!=null){
+                root.right.next=root.next.left;
             }
         }
+        connect(root.left);
+        connect(root.right);
+
         return root;
     }
 }
