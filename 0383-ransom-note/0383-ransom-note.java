7562d75ff52d0cdf578852e48fe53d3a9b34ca23
Piyush Raj Anand <piyushrajanand03@gmail.com>
2026-07-25 21:18:23 +0530

    Time: 1 ms (99.98%), Space: 46.6 MB (48.89%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map1=new HashMap<>();
-        HashMap<Character,Integer> map2=new HashMap<>();
-        for(int i=0;i<magazine.length();i++){
-            char c=magazine.charAt(i);
-            map1.put(c,map1.getOrDefault(c,0)+1);
+        int hash[]=new int[26];
+        for(char c:ransomNote.toCharArray()){
+            hash[c-'a']++;
         }
-        for(int i=0;i<ransomNote.length();i++){
-            char c=ransomNote.charAt(i);
-            map2.put(c,map2.getOrDefault(c,0)+1);
+        for(char c:magazine.toCharArray()){
+            hash[c-'a']--;
         }
-        boolean flag=true;
-        for(int i=0;i<ransomNote.length();i++){
-            char c=ransomNote.charAt(i);
-            if(map2.getOrDefault(c,0)>map1.getOrDefault(c,0)){
-                flag=false;
+        for(int n:hash){
+            if(n>0){
+                return false;
             }
         }
-        return flag;
+        return true;
     }
 }
