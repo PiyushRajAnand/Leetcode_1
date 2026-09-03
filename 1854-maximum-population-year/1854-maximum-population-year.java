0be53658a8a0a3b01e961ffa9c9916767f746f80
Piyush Raj Anand <piyushrajanand03@gmail.com>
2026-09-03 12:14:03 +0530

    Time: 0 ms (100%), Space: 43.8 MB (15.25%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,14 +1,15 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-    int pop[] = new int[2051], res = 0;
-    for (var l : logs) {
-        ++pop[l[0]];
-        --pop[l[1]];
+         int p[]=new int[2051];
+         int res=0; // strore the current ealiest year with maximum pollution
+         for(var l:logs){
+            ++p[l[0]];
+            --p[l[1]];
+         }
+         for(int i=1950;i<2051;i++){
+            p[i]+=p[i-1];
+            res=p[i]>p[res]?i:res;
+         }
+         return res;
     }
-    for (int i = 1950; i < 2050; ++i) {
-        pop[i] += pop[i - 1];
-        res = pop[i] > pop[res] ? i : res;
-    }
-    return res;
-}
 }
