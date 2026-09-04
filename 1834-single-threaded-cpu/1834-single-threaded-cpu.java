6a9d3ac487cfb834d6cd8ac76c2326cbc2020bf7
Piyush Raj Anand <piyushrajanand03@gmail.com>
2026-09-04 15:32:34 +0530

    Time: 209 ms (14.52%), Space: 275.3 MB (32.28%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,57 +1,37 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
-
-        int n = tasks.length;
-        int[] sequence = new int[n];
-
-        // Store: [enqueueTime, processingTime, originalIndex]
-        int[][] arr = new int[n][3];
-
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = tasks[i][0];
-            arr[i][1] = tasks[i][1];
-            arr[i][2] = i;
+        int n=tasks.length;
+        int sequence[]=new int[n];
+        int taskList[][]=new int[n][3];
+        for(int i=0;i<n;i++){
+            taskList[i][1]=tasks[i][0];
+            taskList[i][2]=tasks[i][1];
+            taskList[i][0]=i;
         }
-
-        // Sort according to enqueue time
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
-
-        // Store index in arr
-        // Shortest processing time -> smallest original index
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
-            if (arr[a][1] == arr[b][1]) {
-                return Integer.compare(arr[a][2], arr[b][2]);
+        PriorityQueue<Integer> pq=new PriorityQueue<>(
+            (a,b)->{
+                if(taskList[a][2]==taskList[b][2]){
+                    return Integer.compare(taskList[a][0],taskList[b][0]);
+                }
+                return Integer.compare(taskList[a][2],taskList[b][2]);
             }
-            return Integer.compare(arr[a][1], arr[b][1]);
-        });
-
-        long currentTime = 0;
-        int i = 0;
-        int index = 0;
-
-        while (i < n || !pq.isEmpty()) {
-
-            // If no task is available, jump to next task's enqueue time
-            if (pq.isEmpty()) {
-                currentTime = Math.max(currentTime, arr[i][0]);
+        );
+        Arrays.sort(taskList,(a,b)->a[1]-b[1]);
+        int i=0;
+        int currentTime=0;
+        int index=0;
+        while(i<tasks.length || !pq.isEmpty()){
+            if(pq.isEmpty()){
+                currentTime=Math.max(currentTime,taskList[i][1]);
             }
-
-            // Add all available tasks
-            while (i < n && arr[i][0] <= currentTime) {
+            while(i<n && taskList[i][1]<=currentTime){
                 pq.offer(i);
                 i++;
             }
-
-            // Pick shortest processing time
-            int task = pq.poll();
-
-            // Store ORIGINAL index
-            sequence[index++] = arr[task][2];
-
-            // Process task
-            currentTime += arr[task][1];
+            int t=pq.poll();
+            sequence[index++]=taskList[t][0];
+            currentTime+=taskList[t][2];
         }
-
         return sequence;
     }
 }
