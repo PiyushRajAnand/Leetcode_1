b894de12d8011c0ab0c23e896ec8e903ed827045
Piyush Raj Anand <piyushrajanand03@gmail.com>
2026-09-16 10:57:01 +0530

    Time: 85 ms (78.19%), Space: 50.2 MB (34.25%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.java
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -1,30 +1,44 @@
 class Solution {
     public int openLock(String[] deadends, String target) {
-        Set<String> deadSet = new HashSet<>(Arrays.asList(deadends));
-        if (deadSet.contains("0000")) return -1;
-        Queue<String> q = new LinkedList<>(Collections.singletonList("0000"));
-        for (int steps = 0; !q.isEmpty(); ++steps) {
-            for (int i = q.size(); i > 0; --i) {
-                String curr = q.poll();
-                if (curr.equals(target)) return steps;
-                for (String nei : neighbors(curr)) {
-                    if (deadSet.contains(nei)) continue;
-                    deadSet.add(nei); // Marked as visited
-                    q.offer(nei);
+        Set<String> dead=new HashSet<>(Arrays.asList(deadends));
+        if(dead.contains("0000")){
+            return -1;
+        }
+        Queue<Pair> queue=new ArrayDeque<>();
+        Set<String> vis=new HashSet<>();
+        queue.offer(new Pair(0,"0000"));
+        vis.add("0000");
+        while(!queue.isEmpty()){
+            Pair pair=queue.poll();
+            String str=pair.str;
+            int step=pair.step;
+            if(str.equals(target)){
+                return step;
+            }
+            for(int i=0;i<4;i++){
+                for(int j:new int[]{-1,1}){
+                    char []ch=str.toCharArray();
+                    int c=(ch[i]-'0'+j+10)%10;
+                    ch[i]=(char)(c+'0');
+                    String newStr=String.valueOf(ch);
+
+                    if(!vis.contains(newStr) && !dead.contains(newStr)){
+                        vis.add(newStr);
+                        queue.offer(new Pair(step+1,newStr));
+                        // System.out.print(newStr);
+                    }
                 }
             }
-        }
+        } 
         return -1;
     }
-    List<String> neighbors(String code) {
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            int x = code.charAt(i) - '0';
-            for (int diff = -1; diff <= 1; diff += 2) {
-                int y = (x + diff + 10) % 10;
-                result.add(code.substring(0, i) + ("" + y) + code.substring(i + 1));
-            }
-        }
-        return result;
+}
+
+class Pair{
+    int step;
+    String str;
+    Pair(int step,String str){
+        this.step=step;
+        this.str=str;
     }
 }
