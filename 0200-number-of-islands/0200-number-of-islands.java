491aeed83258b5c03776a470a6f958e8687bf38e
Piyush Raj Anand <piyushrajanand03@gmail.com>
2026-09-20 11:18:42 +0530

    Time: 3 ms (87.28%), Space: 52.6 MB (21.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,33 +2,25 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        boolean isVisited[][]=new boolean[n][m];
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(isVisited[i][j]==false && grid[i][j]=='1'){
+                if(grid[i][j]=='1'){
                     count++;
-                    solve(grid,isVisited,i,j,n,m);
+                    solve(grid,i,j,n,m);
                 }
             }
         }
         return count;
     }
-    public void solve(char[][] grid,boolean[][] isVisited,int sr,int sc,int n,int m){
-        Queue<int[]> queue=new LinkedList<>();
-        isVisited[sr][sc]=true;
-        queue.add(new int[]{sr,sc});
-        while(!queue.isEmpty()){
-            int curr[]=queue.poll();
-            int directions[][]={{0,-1},{0,1},{1,0},{-1,0}};
-            for(int[] dir:directions){
-                int r=curr[0]+dir[0];
-                int c=curr[1]+dir[1];
-                if(r>=0 && c>=0 && r<n && c<m && isVisited[r][c]==false && grid[r][c]=='1'){
-                    isVisited[r][c]=true;
-                    queue.add(new int[]{r,c});
-                }
-            }
+    public void solve(char[][] grid,int sr,int sc,int n,int m){
+        if(sr<0 || sc<0 || sr>=n || sc>=m || grid[sr][sc]=='0'){
+            return;
         }
+        grid[sr][sc]='0';
+        solve(grid,sr-1,sc,n,m);
+        solve(grid,sr+1,sc,n,m);
+        solve(grid,sr,sc-1,n,m);
+        solve(grid,sr,sc+1,n,m);
     }
 }
